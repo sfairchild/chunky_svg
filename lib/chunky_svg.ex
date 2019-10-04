6974537966315de995004a02d858f1c8d0140bad
Sean <seanf215@gmail.com>
2019-10-04 16:49:11 -0400

    allow optional svg tag attributes to be passed

--- lib/chunky_svg.ex
+++ lib/chunky_svg.ex
@@ -1,16 +1,14 @@
 defmodule ChunkySVG do
-  def render(drawing) when is_tuple(drawing) do
-    render([drawing])
-  end
+  def render(drawing) when is_tuple(drawing), do: render([drawing], %{})
+  def render(drawing, svg_options) when is_tuple(drawing), do: render([drawing], svg_options)
+  def render(drawing), do: render(drawing, %{})
 
-  def render(drawing) when is_list(drawing) do
+  def render(drawing, svg_options) when is_list(drawing) and is_map(svg_options) do
+    svg_options = %{viewBox: "0 0 100 100", xmlns: "http://www.w3.org/2000/svg"}
+                  |> Map.merge(svg_options)
     drawing = drawing |>
       ChunkySVG.Builtin.expand |>
       ChunkySVG.InlineMacro.expand
-    {
-      :svg,
-      %{viewBox: "0 0 100 100", xmlns: "http://www.w3.org/2000/svg"},
-      drawing
-    } |> XmlBuilder.generate
+    { :svg, svg_options, drawing } |> XmlBuilder.generate
   end
 end
